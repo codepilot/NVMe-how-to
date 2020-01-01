c57914e2361005cfaf970f16cc89c9a110c708dc
Daniel Kluss <codepilot@gmail.com>
2020-01-01 12:30:47 -0800

    made functions instead of static methods

--- JavaScript/NVMe.js
+++ JavaScript/NVMe.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const {
-  regs,
-  subregs,
-} = require('./regs');
-
 function readAll(obj) {
   if(Array.isArray(obj)) { return obj; }
   if(typeof(obj) === 'object') {
@@ -17,105 +12,108 @@ function makeObject(descriptorEntries) {
   return Object.freeze(Object.create(null, Object.fromEntries(descriptorEntries)));
 }
 
-module.exports.NVMe = class NVMe {
-  static defineSubregisters({registerBuffer, k}) {
-    const subregister_descriptor_entries = Object.entries(subregs[k]).map(([sr, {area, RO, mapping}]) => {
-      if(RO) {
-        area = Array.isArray(area)?area:[area, area];
-        const shift = area[1];
-        const bits = 1n + (area[0] - shift);
-        const mask = (1n << bits) - 1n;
+function defineSubregisters({registerBuffer, subregs, k}) {
+  const subregister_descriptor_entries = Object.entries(subregs[k]).map(([sr, {area, RO, mapping}]) => {
+    if(RO) {
+      area = Array.isArray(area)?area:[area, area];
+      const shift = area[1];
+      const bits = 1n + (area[0] - shift);
+      const mask = (1n << bits) - 1n;
 
-        if(registerBuffer instanceof Uint32Array) {
-          const descriptor = {
-            enumerable: true,
-            get: ()=> {
-              const rawValue = (BigInt(registerBuffer[0]) >> shift) & mask;
-              let value = mapping({value: rawValue, area});
-              return value;
-            }
-          };
-          return [sr, descriptor];
-        }
-        if(registerBuffer instanceof BigUint64Array) {
-          const descriptor = {
-            enumerable: true,
-            get: ()=> {
-              const rawValue = (registerBuffer[0] >> shift) & mask;
-              let value = mapping({value: rawValue, area});
-              return value;
-            }
-          };
-          return [sr, descriptor];
-        }
+      if(registerBuffer instanceof Uint32Array) {
+        const descriptor = {
+          enumerable: true,
+          get: ()=> {
+            const rawValue = (BigInt(registerBuffer[0]) >> shift) & mask;
+            let value = mapping({value: rawValue, area});
+            return value;
+          }
+        };
+        return [sr, descriptor];
       }
-    });
-    return makeObject(subregister_descriptor_entries);
-  }
-
-  static createBufferAccessorsUI32({registerBuffer}) {
-    const descriptor = {
-      get: ()=> BigInt(registerBuffer[0]),
-      set: (v)=> registerBuffer[0] = Number(v),
-    };
-    return descriptor;
-  }
-
-  static createBufferAccessorsUI64({registerBuffer}) {
-    const descriptor = {
-      get: ()=> registerBuffer[0],
-      set: (v)=> registerBuffer[0] = v,
-    };
-    return descriptor;
-  }
-
-  static createBufferAccessors({registerBuffer, start, end}) {
-    const byteSize = (end - start) + 1n;
-    if(byteSize === 4n) {
-      return NVMe.createBufferAccessorsUI32({registerBuffer, start});
+      if(registerBuffer instanceof BigUint64Array) {
+        const descriptor = {
+          enumerable: true,
+          get: ()=> {
+            const rawValue = (registerBuffer[0] >> shift) & mask;
+            let value = mapping({value: rawValue, area});
+            return value;
+          }
+        };
+        return [sr, descriptor];
+      }
     }
-    if(byteSize === 8n) {
-      return NVMe.createBufferAccessorsUI64({registerBuffer, start});
-    }
+  });
+  return makeObject(subregister_descriptor_entries);
+}
+
+function createBufferAccessorsUI32({registerBuffer}) {
+  const descriptor = {
+    get: ()=> BigInt(registerBuffer[0]),
+    set: (v)=> registerBuffer[0] = Number(v),
+  };
+  return descriptor;
+}
+
+function createBufferAccessorsUI64({registerBuffer}) {
+  const descriptor = {
+    get: ()=> registerBuffer[0],
+    set: (v)=> registerBuffer[0] = v,
+  };
+  return descriptor;
+}
+
+function createBufferAccessors({registerBuffer, start, end}) {
+  const byteSize = (end - start) + 1n;
+  if(byteSize === 4n) {
+    return createBufferAccessorsUI32({registerBuffer, start});
   }
-
-  static createBuffer({srcBuf, start, end}) {
-    const byteSize = (end - start) + 1n;
-    if(byteSize === 4n) {
-      return new Uint32Array(srcBuf, Number(start), 1);
-    }
-    if(byteSize === 8n) {
-      return new BigUint64Array(srcBuf, Number(start), 1);
-    }
+  if(byteSize === 8n) {
+    return createBufferAccessorsUI64({registerBuffer, start});
   }
+}
 
-  static defineRegisters({srcBuf}) {
-    const registerBuffers = makeObject(Object.entries(regs).map(([k, {start, end}])=> [k, {
-      enumerable: true,
-      value: NVMe.createBuffer({k, srcBuf, start, end}),
-    }]));
-
-    const registers = makeObject(Object.entries(regs).map(([k, {start, end}])=> [k, {
-      enumerable: true,
-      ...NVMe.createBufferAccessors({k, registerBuffer: registerBuffers[k], start, end}),
-    }]));
-
-    const subregisters = makeObject(Object.keys(subregs).map(k=> [k, {
-      enumerable: true,
-      value: NVMe.defineSubregisters({registerBuffer: registerBuffers[k], k}),
-    }]));
-
-    return {registers, registerBuffers, subregisters};
+function createBuffer({srcBuf, start, end}) {
+  const byteSize = (end - start) + 1n;
+  if(byteSize === 4n) {
+    return new Uint32Array(srcBuf, Number(start), 1);
   }
+  if(byteSize === 8n) {
+    return new BigUint64Array(srcBuf, Number(start), 1);
+  }
+}
 
+function defineRegisters({srcBuf}) {
+  const {
+    regs,
+    subregs,
+  } = require('./regs');
+
+  const registerBuffers = makeObject(Object.entries(regs).map(([k, {start, end}])=> [k, {
+    enumerable: true,
+    value: createBuffer({k, srcBuf, start, end}),
+  }]));
+
+  const registers = makeObject(Object.entries(regs).map(([k, {start, end}])=> [k, {
+    enumerable: true,
+    ...createBufferAccessors({k, registerBuffer: registerBuffers[k], start, end}),
+  }]));
+
+  const subregisters = makeObject(Object.keys(subregs).map(k=> [k, {
+    enumerable: true,
+    value: defineSubregisters({registerBuffer: registerBuffers[k], subregs, k}),
+  }]));
+
+  return {registers, registerBuffers, subregisters};
+}
+
+module.exports.NVMe = class NVMe {
   constructor(options={/*Driver, PCIeDev*/}) {
     Object.assign(this, options);
 
-    Object.defineProperty(this, 'bar01', {value: options.PCIeDev.Bar01.buffer});
-    Object.defineProperty(this, 'bar01_ui32', {value: new Uint32Array(this.bar01)});
-    Object.defineProperty(this, 'bar01_ui64', {value: new BigUint64Array(this.bar01)});
+    const bar01 = options.PCIeDev.Bar01.buffer;
 
-    const {registers, registerBuffers, subregisters} = NVMe.defineRegisters({srcBuf: this.bar01});
+    const {registers, registerBuffers, subregisters} = defineRegisters({srcBuf: bar01});
 
     const submission = [];
     const completion = [];
@@ -125,10 +123,11 @@ module.exports.NVMe = class NVMe {
       for(let y = 0n; y < 65536n; y++) {
         const submission_offset = 0x1000n + (2n * y) * cached_DSTRD;
         const completion_offset = submission_offset + cached_DSTRD;
-        const submission_ui32Index = Number(submission_offset >> 2n);
-        const completion_ui32Index = Number(completion_offset >> 2n);
-        submission[y] = this.bar01_ui32.slice(submission_ui32Index, submission_ui32Index + 1);
-        completion[y] = this.bar01_ui32.slice(completion_ui32Index, completion_ui32Index + 1);
+        if(submission_offset + cached_DSTRD * 2n > bar01.byteLength) {
+          break;
+        }
+        submission[y] = new Uint32Array(bar01, Number(submission_offset), 1);
+        completion[y] = new Uint32Array(bar01, Number(completion_offset), 1);
       }
     }
 
@@ -143,11 +142,7 @@ module.exports.NVMe = class NVMe {
     });
 
     Object.freeze(this);
-    //console.log(this);
     console.log(readAll(registers));
-    console.log(readAll(registerBuffers));
     console.log(readAll(subregisters));
-
-
   }
 };
